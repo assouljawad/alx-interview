76f53ec575c37fd59d95c6d4b98860890d723c12
assouljawad <38533122+assouljawad@users.noreply.github.com>
2024-01-05 00:50:05 +0100

    fix row var

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
 def pascal_triangle(n):
-
-    if n <= 0:
-        return []
-
+    """returns the pascal triangle"""
     triangle = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
 
+    if n < 1:
+        return triangle
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            som = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(som)
+        row.append(1)
+        triangle.append(row)
     return triangle
