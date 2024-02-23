e29f1ff59ca87781776577f17062a63698b8d4cd
assouljawad <38533122+assouljawad@users.noreply.github.com>
2024-02-23 01:02:33 +0100

    all good

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-"""Rotates a 2D matrix"""
-
+"""
+Rotate 2D Matrix
+"""
 
 def rotate_2d_matrix(matrix):
     """
-       Rotates 2D matrix 90 degrees clockwise
-       Matrix is edited in-place
-       args:
-          matrix
+    Rotate a 2D matrix 90 degrees clockwise in-place.
+    :param matrix: List of lists representing the 2D matrix
+    :return: None
     """
-    left, right = 0, len(matrix) - 1
+    # Step 1: Transpose the matrix
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    while left < right:
-        for i in range(right - left):
-            top, bottom = left, right
-            # save topleft  value
-            topLeft = matrix[top][left + i]
-            # move bottom left to top left
-            matrix[top][left + i] = matrix[bottom - i][left]
-            # move bottom right to bottom left
-            matrix[bottom - i][left] = matrix[bottom][right - i]
-            # move top right to bottom right
-            matrix[bottom][right - i] = matrix[top + i][right]
-            # move top left to top right
-            matrix[top + i][right] = topLeft
-        right -= 1
-        left += 1
+    # Step 2: Reverse each row of the transposed matrix
+    for i in range(n):
+        matrix[i].reverse()
