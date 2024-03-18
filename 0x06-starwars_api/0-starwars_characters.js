dbb8617d17e558534d9cb7c086d7b9a27e4e05c8
assouljawad <assouljawad823@gmail.com>
2024-03-18 17:31:32 +0100

    fix code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,28 @@
 #!/usr/bin/node
 
-const request = require('request');
+const util = require('util');
+const request = util.promisify(require('request'));
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
-
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+async function call () {
+  const num = process.argv.slice(2);
+  if (num.length === 0) {
+    return;
   }
-);
+  const url = `https://swapi-api.alx-tools.com/api/films/${num[0]}`;
+  try {
+    let req = await request(url);
+    const characters = await JSON.parse(req.body).characters;
+    if (characters === undefined) {
+      return;
+    }
+    for (let i = 0; i < characters.length; i++) {
+      req = await request(characters[i]);
+      const character = await JSON.parse(req.body);
+      console.log(character.name);
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+call();
